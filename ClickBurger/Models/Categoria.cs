15b7eb749d088b8e594773c5d4eb8856367ffc71
Jovana Matos <jovanapereira2209@gmail.com>
2024-08-07 15:22:42 +0100

    Definindo propiedades entre as classes...

--- ClickBurger/Models/Categoria.cs
+++ ClickBurger/Models/Categoria.cs
@@ -1,9 +1,21 @@
-﻿namespace ClickBurger.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClickBurger.Models
 {
+    [Table("Categorias")]//Apesar de não ser necessário mais, defino que a class será mapeada para tabela Categorias
     public class Categoria
     {
+        [Key]//reforçando que será nossa key msm sabendo que a palabra ID por conveção ja se entende que será a chave
         public int CategoriaId { get; set; }
-        public string CategoriaName { get; set; }
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]//aqui defino o tamanho max com uma msg de erro
+        [Required(ErrorMessage = "Informe o nome da categoria")]//definindo que este campo seja obrigatório e not null no campo
+        [Display(Name = "Nome")]//apliquei para exibir o atributo nome
+        public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 100 caracteres")]//aqui defino o tamanho max com uma msg de erro
+        [Required(ErrorMessage = "Informe a descrição da categoria")]//definindo que este campo seja obrigatório e not null no campo
+        [Display(Name = "Nome")]//apliquei para exibir o atributo nome
         public string Descricao { get; set; }
 
 
--- ClickBurger/Models/Hamburguer.cs
+++ ClickBurger/Models/Hamburguer.cs
@@ -1,18 +1,49 @@
-﻿namespace ClickBurger.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClickBurger.Models
 {
+    [Table("Hamburgueres")]
     public class Hamburguer
     {
+        [Key]
         public int HamburguerId { get; set; }
+        [Required(ErrorMessage ="O nome do lanche deve ser informado")]
+        [Display (Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength =10, ErrorMessage ="O {0} deve ter no minimo {1} e no máximo {2}")] //stringLength permite eu defini a quantidade max e minima
         public string Nome { get; set; }
+
+        
+        [Required(ErrorMessage = "A descrição do hamburguer deve ser informada")]
+        [Display(Name = "Descrição do Hamburguer")]
+        [MinLength(20, ErrorMessage ="Descrição do deve ter no minimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]// usamos o max e min apenas para mostrar que podemos usala-los porem pode ser feito tudo pelo StringLength
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descrição do hamburguer deve ser informada")]
+        [Display(Name = "Descrição do Hamburguer")]
+        [MinLength(20, ErrorMessage = "Descrição do deve ter no minimo {1} caracteres")]
+        [MaxLength(300, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
 
+        [Required (ErrorMessage ="Informe o preço do hamburguer")]
+        [Display(Name = "Preço")]
+        [Column(TypeName ="decimal (10,2")]//definindo o tipo e a precisão
+        [Range(1,999.99,ErrorMessage = "O preço deve estar entre 1 e 999,99")]
         public decimal Preco {  get; set; }
-        public string ImagemUrl { get; set; }
-        public string ImagemThumbnaiUrl { get; set; }
-        public bool IsHamburguerPreferido { get; set; }
-        public bool EmEstoque { get; set; }
 
+        [Display(Name= "Caminho Imagem miniatura")]
+        [StringLength(200, ErrorMessage ="O {0} deve ter no máximo {1} caracteres")]
+        public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho Imagem miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+        public string ImagemThumbnaiUrl { get; set; }
+
+        [Display(Name = "Preferido")]
+        public bool IsHamburguerPreferido { get; set; }
+        [Display(Name = "Estoque")]
+        public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; } //definindo o relacionamento
         public virtual Categoria Categoria { get; set; }
